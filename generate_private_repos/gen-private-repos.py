42b149891de31d08409f598d5b35b5e0c23f5a9b
Kim Trang <kim.trang@couchbase.com>
2019-02-01 17:34:55 -0800

    Moving to class

--- generate_private_repos/gen-private-repos.py
+++ generate_private_repos/gen-private-repos.py
@@ -1,61 +1,30 @@
 #!/usr/bin/env python3.6
-
-
 import argparse
 import configparser
-from collections import defaultdict
-from lxml import etree
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 import sys
 
+from collections import defaultdict
 
-def initialize_etree(input):
-    tree = etree.parse(input)
-    return tree
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
 
+import cbbuild.manifest.info as cb_info
+import cbbuild.manifest.parse as cb_parse
 
-def get_remote(tree):
-    ''' Parse manifest to get ssh remote
-        It could be more than one ssh remote url
-        return private_remote dictionary
-        'couchbase-priv': 'ssh://git@github.com/...'
-    '''
-    private_remote = {}
-    remote_node = tree.iterfind("//remote")
-
-    for remote in remote_node:
-        remote_name = remote.get('name')
-        remote_fetch = remote.get('fetch', remote_name)
-        if 'ssh' in remote_fetch:
-            private_remote[remote_name] = remote_fetch
-    return private_remote
+from pprint import pprint
 
 
-def get_repos(remotes, tree):
-    ''' Parse manifest to get all private repo names
-        return dictionary of repos with remote name
-        'voltron': 'couchbase-priv' ...
-    '''
-    private_repos = {}
-    projects = tree.iterfind("//project")
+def get_project_info(input):
+    manifest = cb_parse.Manifest(input)
+    manifest_data = manifest.parse_data()
+    manifest_info = cb_info.ManifestInfo(manifest_data)
+    priv_urls = dict()
+    for p in manifest_info.get_projects():
+        remote, url = manifest_info.get_project_remote_info(p)
+        if url.startswith('ssh'):
+            priv_urls[p] = url.replace('ssh://git@', 'https://')
 
-    remote_names = remotes.keys()
-    for proj in projects:
-        proj_name = proj.get('name')
-        proj_remote = proj.get('remote', proj_name)
-        for remote in remote_names:
-            if remote == proj_remote:
-                private_repos[proj_name] = proj_remote
-    return private_repos
-
-
-def generate_remote_path(repos, remotes):
-    ''' get remote ssh:// translate to http:// '''
-    remote_path = {}
-    for url in remotes:
-        remote_path[url] = remotes[url].replace('ssh://git@', 'http://')
-    return remote_path
+    return priv_urls
 
 
 def read_projects_config(conf_file):
@@ -74,17 +43,18 @@ def read_projects_config(conf_file):
     except IOError as error:
         sys.exit(error)
     else:
-        projects = config.sections()
+        project_groups = config.sections()
 
-    for proj in projects:
-        proj_list = []
-        for key in config[proj]:
-            proj_list.append(key)
-        projects_config[proj] = proj_list
+    for proj in project_groups:
+        # list of tuples: [('cbas', ''), ('cbas-core', '')]
+        projects_tuples = config.items(proj)
+        projects = [x[0] for x in projects_tuples]
+        projects_config[proj] = projects
     return projects_config
+    # return {pgroup: config[pgroup] for pgroup in project_groups}
 
 
-def generate_report(config, repos, rpath, outfile):
+def generate_report(config, priv_path_repo, outfile):
     '''Generate report text file '''
     project_url = defaultdict(list)
     projects = read_projects_config(config)
@@ -94,23 +64,26 @@ def generate_report(config, repos, rpath, outfile):
         'http://github.com/couchbase/cbas-core': 'Analytics'
         'http://github.com/couchbase/cbas': 'Analytics', ...
     '''
-    for repo in repos:
+    print(type(projects))
+    pprint(projects)
+    for repo in priv_path_repo:
         for proj, proj_config_repos in projects.items():
             if repo in proj_config_repos:
-                project_url[rpath[repos[repo]] + repo] = proj
+                project_url[priv_path_repo[repo]] = proj
 
+    pprint(project_url)
     ''' Check if repos has not been define in project config category
         bailed out so private repos can be added to category in projects.ini
     '''
-    repo_names = sorted(repos.keys())
+    repo_names = sorted(priv_path_repo.keys())
     lists_of_projects = sorted(projects.values())
     # flatten out the lists_of_projects in projects.ini
     proj_flat_list = [item for llist in lists_of_projects for item in llist]
     found_missing_products = set(repo_names).difference(proj_flat_list)
     if found_missing_products:
         print("\n\n")
-        print("=== Found private repos missing in %s! ===" % conf)
-        print("=== Please add the missing repo(s) in %s file ===!" % conf)
+        print("=== Found private repos missing in %s! ===" % config)
+        print("=== Please add the missing repo(s) in %s file ===!" % config)
         print('\n'.join(found_missing_products))
         sys.exit(1)
 
@@ -148,8 +121,10 @@ def ListFolder(parent_folderid, drive):
     else:
         for f in file_list:
             if f['mimeType'] == 'application/vnd.google-apps.folder':  # if folder
+                print("AM I IN FOLDER IF?")
                 filelist.append({"id": f['id'], "title": f['title'], "list": ListFolder(f['id'])})
             else:
+                print("AM I IN FOLDER ELSE?")
                 filelist[f['title']] = f['id']
     return filelist
 
@@ -191,15 +166,14 @@ def gdrive_upload(drive, gdrive_files, folder_id, upload_file):
 
 def main(args):
     output_file = f'{args.release}.txt'
-    tree = initialize_etree(args.input)
-    remotes = get_remote(tree)
-    repos = get_repos(remotes, tree)
-    private_remote_path = generate_remote_path(repos, remotes)
-    generate_report(args.conf, repos, private_remote_path, output_file)
+    private_remote_path = get_project_info(args.input)
+    print(private_remote_path)
+    generate_report(args.conf, private_remote_path, output_file)
     # Upload to gdrive
     drive = g_authenticate()
     filelist = ListFolder(args.folder_id, drive)
-    gdrive_upload(drive, filelist, args.folder_id, output_file)
+    pprint(filelist)
+    #gdrive_upload(drive, filelist, args.folder_id, output_file)
 
 
 if __name__ == "__main__":
