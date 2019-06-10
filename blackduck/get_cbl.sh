88377d83506847c09201ad96d7d1b74498b9e18e
Kim Trang <kim.trang@couchbase.com>
2019-06-10 11:51:07 -0700

    Remove dotnet

--- blackduck/get_cbl.sh
+++ blackduck/get_cbl.sh
@@ -18,6 +18,7 @@ repo forall -c 'git submodule update --init --recursive'
 
 # Ignore source/directories
 # added ignore vendor/mbedtls per https://issues.couchbase.com/browse/CBD-2828
-for i in .repo .git fleece gradle Docs doc test tests docs googletest third_party _obsolete mbedtls VisualStudio dotzlib; do
+#for i in .repo .git fleece gradle Docs doc test tests docs googletest third_party _obsolete mbedtls VisualStudio dotzlib; do
+for i in .repo .git fleece gradle Docs doc test tests docs googletest third_party _obsolete; do
    find . -type d -name $i | xargs rm -rf
 done
