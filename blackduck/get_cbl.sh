ecc5183ef814630eeeb7546d41b0253368a08fa2
Kim Trang <kim.trang@couchbase.com>
2019-06-24 13:20:39 -0700

    Update scan script

--- blackduck/get_cbl.sh
+++ blackduck/get_cbl.sh
@@ -18,7 +18,6 @@ repo forall -c 'git submodule update --init --recursive'
 
 # Ignore source/directories
 # added ignore vendor/mbedtls per https://issues.couchbase.com/browse/CBD-2828
-#for i in .repo .git fleece gradle Docs doc test tests docs googletest third_party _obsolete mbedtls VisualStudio dotzlib; do
-for i in .repo .git fleece gradle Docs doc test tests docs googletest third_party _obsolete mbedtls; do
+for i in .repo .git fleece gradle Docs doc test tests docs googletest third_party _obsolete mbedtls VisualStudio dotzlib; do
    find . -type d -name $i | xargs rm -rf
 done
