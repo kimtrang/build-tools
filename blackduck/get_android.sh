0d08d4c4f7f99cb8e2daf0929a85751acbbec494
Kim Trang <kim.trang@couchbase.com>
2019-07-17 15:43:40 -0700

    Update

--- blackduck/get_android.sh
+++ blackduck/get_android.sh
@@ -39,9 +39,9 @@ cp local.properties ${WORKSPACE}/local.properties
 
 export PATH=$PATH:/usr/local/go/bin
 bash <(curl -s https://detect.synopsys.com/detect.sh) \
---detect.project.name=${PRODUCT}" \
+--detect.project.name=${PRODUCT} \
 --detect.project.version.name=${VERSION} \
---detect.project.codelocation.prefix="PRODUCT" \
+--detect.project.codelocation.prefix=${PRODUCT} \
 --detect.source.path=${WORKSPACE} \
 --logging.level.com.synopsys.integration=DEBUG \
 --blackduck.url=https://blackduck.build.couchbase.com \
